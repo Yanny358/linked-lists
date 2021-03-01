92325382efb7916a4fce2e40ce8082d424d0f6b6
jametl <jametl@ttu.ee>
2021-03-01 12:38:57 +0200

    NewStack,Tos,push,pop are done

--- src/LongStack.java
+++ src/LongStack.java
@@ -1,37 +1,61 @@
+import java.util.EmptyStackException;
+
+// A Linked List Node
+class Node
+{
+   long data;       // integer data
+   Node next;      // pointer to the next node
+}
+
 
 public class LongStack {
+
+   private Node top;
 
    public static void main (String[] argum) {
       // TODO!!! Your tests here!
    }
 
    LongStack() {
-      // TODO!!! Your constructor here!
+      this.top = null;
    }
 
    @Override
    public Object clone() throws CloneNotSupportedException {
-      return this; // TODO!!! Your code here!
+      return this;
    }
 
    public boolean stEmpty() {
-      return false; // TODO!!! Your code here!
+      return top == null;
    }
 
    public void push (long a) {
-      // TODO!!! Your code here!
+      Node node = new Node();
+      node.data = a;
+      node.next = top;
+      top = node;
    }
 
-   public long pop() {
-      return 0; // TODO!!! Your code here!
-   } // pop
+   public long pop(){
+      // check for stack underflow
+      if (top == null)
+      {
+         throw new EmptyStackException();
+      }
+      long data = top.data;
+      top = top.next;
+      return data;
+   }
 
    public void op (String s) {
       // TODO!!!
    }
   
    public long tos() {
-      return 0; // TODO!!! Your code here!
+      if (!stEmpty()){
+         return top.data;
+      }
+      else throw new RuntimeException();
    }
 
    @Override
