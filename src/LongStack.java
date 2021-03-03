7697ce209763f88db83ecd45d51a9e1046846f6f
jametl <jametl@ttu.ee>
2021-03-03 12:51:22 +0200

    Clone,equal,toString are done

--- src/LongStack.java
+++ src/LongStack.java
@@ -1,49 +1,86 @@
 import java.util.EmptyStackException;
 
-// A Linked List Node
+
 class Node
 {
-   long data;       // integer data
-   Node next;      // pointer to the next node
+   long data;
+   Node next;
+
+   public Node(long data) {
+      this.data = data;
+      this.next = null;
+   }
+   public Node(long data,Node next) {
+      this.data = data;
+      this.next = next;
+   }
+   public Node() {}
 }
 
 
 public class LongStack {
 
-   private Node top;
+   private Node head;
+
+   public void display(){
+      if (this.head == null){
+         System.out.println("Stack is empty!");
+      }
+      Node temp = this.head;
+      while (temp != null){
+         System.out.println(temp.data);
+         temp = temp.next;
+      }
+   }
 
    public static void main (String[] argum) {
-      // TODO!!! Your tests here!
+      LongStack m1 = new LongStack();
+      m1.push (5);
+      m1.push (4);
+      LongStack m2 = null;
+      try {
+         m2 = (LongStack)m1.clone();
+      } catch (CloneNotSupportedException e) {};
+      m1.display();
+      System.out.println("----------");
+      m2.display();
+      System.out.println(m1);
+      System.out.println(m2);
    }
 
    LongStack() {
-      this.top = null;
+      this.head = null;
    }
 
    @Override
    public Object clone() throws CloneNotSupportedException {
-      return this;
+      LongStack tmp = new LongStack();
+      Node ptr = this.head;
+      while (ptr != null){
+         tmp.head = head;
+         ptr = ptr.next;
+      }
+      return tmp;
    }
 
    public boolean stEmpty() {
-      return top == null;
+      return head == null;
    }
 
    public void push (long a) {
-      Node node = new Node();
-      node.data = a;
-      node.next = top;
-      top = node;
+      Node node = new Node(a);
+      node.next = this.head;
+      this.head = node;
    }
 
    public long pop(){
       // check for stack underflow
-      if (top == null)
+      if (head == null)
       {
          throw new EmptyStackException();
       }
-      long data = top.data;
-      top = top.next;
+      long data = head.data;
+      head = head.next;
       return data;
    }
 
@@ -53,19 +90,55 @@ public class LongStack {
   
    public long tos() {
       if (!stEmpty()){
-         return top.data;
+         return head.data;
       }
       else throw new RuntimeException();
    }
 
    @Override
    public boolean equals (Object o) {
-      return true; // TODO!!! Your code here!
+      Node ptr = this.head;
+      if (head == null && ((LongStack) o).head != null){
+         return false;
+      }
+      while (ptr != null && ((LongStack) o).head != null){
+         if (((LongStack) o).head.data != head.data){
+            return false;
+         }
+         if (((LongStack) o).head.next != head.next ){
+            return false;
+         }
+
+         ptr = ptr.next;
+
+      }
+      return true;
    }
 
    @Override
    public String toString() {
-      return null; // TODO!!! Your code here!
+      StringBuilder sb = new StringBuilder();
+      sb.append("-->");
+
+      Node curr = this.head;
+
+      if (curr == null)
+      {
+         sb.append("<--");
+         return sb.toString();
+      }
+
+      sb.append(curr.data);
+      curr = curr.next;
+      while(curr != null) {
+         sb.append("<-->");
+         sb.append(curr.data);
+         curr = curr.next;
+      }
+      sb.append("<--");
+
+      return sb.toString();
+      /* https://stackoverflow.com/questions/37766590/implementing-a-tostring-method-to-print-out-a-linkedlist */
    }
 
    public static long interpret (String pol) {
